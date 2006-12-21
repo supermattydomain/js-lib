99cd091fa2a54b60e2a4dcc363dfe2396be323b7
(no author) <(no author)@d75fb2d8-f9ea-0310-9219-f9bc96eeea74>
2006-12-21 07:46:55 +0000

    Add an ArrayIter class.
    git-svn-id: https://svn/svn/js-lib@696 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

---
+++ iter.js
@@ -0,0 +1,26 @@
+function arrayForAll(array, elementCallback, args) {
+  // printMessage('arrayForAll(' + array.toString() + ', ' + args.toString() + ')');
+  var i;
+  for (i = 0; i < array.length; i++) {
+    args.push(array[i]);
+    elementCallback(args);
+    args.pop();
+  }
+}
+
+function ArrayIter(array) {
+  this.iterArray = array;
+  this.iterIndex = 0;
+  this.getCount = function() {
+    return this.iterArray.length;
+  }
+  this.getNext = function() {
+    if (this.iterIndex >= this.iterArray.lenth) {
+      return null;
+    }
+    return this.iterArray[this.iterIndex++];
+  }
+  this.forAll = function(callback, args) {
+    arrayForAll(this.iterArray, callback, args);
+  }
+}
