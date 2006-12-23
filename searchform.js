31986e813bba8017dfbf41d0f41fbd627b9ec1d3
(no author) <(no author)@d75fb2d8-f9ea-0310-9219-f9bc96eeea74>
2006-12-23 09:46:36 +0000

    Make this class retrieve the XML schema and enumerate over the tables and fields therein.
    git-svn-id: https://svn/svn/js-lib@792 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- searchform.js
+++ searchform.js
@@ -1,100 +1,92 @@
-var num_criteria = 0;
-var max_criteria = 20;
-
-var fieldLabels = null;
-
-function makeFieldLabels() {
-  if (null != fieldLabels) {
-    return;
-  }
-  fieldLabels = new Array();
-  var i;
-  for (i = 0; i < fields.length; i++) {
-    fieldLabels[i] = ucFirstAll(fields[i].replace(/\./g, ' '));
-  }
-}
-
-function makeOption(label, value) {
+function SearchForm() {
+  this.numCriteria = 0;
+  this.maxCriteria = 20;
+  this.fieldLabels = null;
+  this.searchForm = document.getElementById('searchform');
+  var self = this;
+  this.makeFieldLabels = function() {
+    if (null != this.fieldLabels) {
+      return;
+    }
+    this.fieldLabels = new Array();
+    var i;
+    for (i = 0; i < fields.length; i++) {
+      this.fieldLabels[i] = ucFirstAll(fields[i].replace(/\./g, ' '));
+    }
+  };
+  this.makeOption = function(label, value) {
     var option = document.createElement("option");
     option.setAttribute("label", label);
     option.setAttribute("value", value);
     option.appendChild(document.createTextNode(label));
     return option;
-}
-
-function addOption(select, optionName, optionValue) {
-    select.appendChild(makeOption(optionName, optionValue));
-}
-
-function makeSelectOptionIter(name, optionNameIter, optionValueIter) {
+  };
+  this.addOption = function(select, optionName, optionValue) {
+    select.appendChild(this.makeOption(optionName, optionValue));
+  };
+  this.makeSelectOptionIter = function(name, optionNameIter, optionValueIter) {
     var select = document.createElement("select");
     select.setAttribute("name", name);
     while (optionNameIter.hasMore() && optionValueIter.hasMore()) {
-	addOption(select, optionNameIter.getNext(), optionValueIter.getNext());
+	this.addOption(select, optionNameIter.getNext(), optionValueIter.getNext());
     }
     return select;
-}
-
-function makeSelectOptionArray(name, optionNames, optionValues) {
+  };
+  this.makeSelectOptionArray = function(name, optionNames, optionValues) {
     var optionNameIter = new ArrayIter(optionNames);
     var optionValueIter = new ArrayIter(optionValues);
-    return makeSelectOptionIter(name, optionNameIter, optionValueIter);
-}
-
-function makeCriterion(num) {
-    var criterion_div = document.createElement("div");
-    criterion_div.setAttribute("id", "criterion" + num);
-    // criterion_div.style.visible = false;
-    criterion_div.appendChild(makeSelectOptionArray("field" + num, fieldLabels, fields));
-    criterion_div.appendChild(makeSelectOptionArray("operation" + num, operations, operations));
+    return this.makeSelectOptionIter(name, optionNameIter, optionValueIter);
+  };
+  this.makeCriterion = function(num) {
+    var criterionDiv = document.createElement("div");
+    criterionDiv.setAttribute("id", "criterion" + num);
+    // criterionDiv.style.visible = false;
+    criterionDiv.appendChild(this.makeSelectOptionArray("field" + num, this.fieldLabels, fields));
+    criterionDiv.appendChild(this.makeSelectOptionArray("operation" + num, operations, operations));
     var text_field = document.createElement("input");
     text_field.setAttribute("name", "value" + num);
     text_field.setAttribute("type", "text");
-    criterion_div.appendChild(text_field);
-    return criterion_div;
-}
-
-function addCriterion() {
-    if (num_criteria >= max_criteria) {
+    criterionDiv.appendChild(text_field);
+    return criterionDiv;
+  };
+  this.addCriterion = function() {
+    if (this.numCriteria >= this.maxCriteria) {
 	return;
     }
-    makeFieldLabels();
-    var search_form = document.getElementById("searchform");
-    search_form.appendChild(makeCriterion(num_criteria));
-    num_criteria++;
-}
-
-function removeCriterion() {
-    if (num_criteria < 1) {
+    this.makeFieldLabels();
+    this.searchForm.appendChild(this.makeCriterion(this.numCriteria));
+    this.numCriteria++;
+  };
+  this.removeCriterion = function() {
+    if (this.numCriteria < 1) {
 	return;
     }
-    var criterion_div = document.getElementById("criterion" + (num_criteria - 1));
-    criterion_div.parentNode.removeChild(criterion_div);
-    num_criteria--;
-}
-
-function receiveFieldForm(args) {
-  var field = args[0];
-  var table = field.parentNode;
-  printMessage('Found field ' + schema.getTableName(table) + '.' + schema.getFieldName(field) + '\n');
-}
-
-function receiveTableForm(args) {
-  var schema = args[0];
-  var table = args[1];
-  printMessage('Found table ' + schema.getTableName(table) + '\n');
-  fieldArgs = new Array();
-  schema.enumFields(table, receiveFieldForm, fieldArgs);
-}
-
-var onSchemaFetchedForm = function() {
-  // printMessage('Request done');
-  var args = new Array();
-  args[0] = schema;
-  schema.enumTables(receiveTableForm, args);
-};
-
-function populateForm() {
-  var schema = new DBSchema();
-  schema.fetchSchema(onSchemaFetchedForm);
+    var criterionDiv = document.getElementById("criterion" + (this.numCriteria - 1));
+    criterionDiv.parentNode.removeChild(criterionDiv);
+    this.numCriteria--;
+  };
+  this.receiveFieldForm = function(args) {
+    var schema = args[0];
+    var field = args[1];
+    var table = field.parentNode;
+    printMessage('Found field ' + schema.getTableName(table) + '.' + schema.getFieldName(field) + '\n');
+  };
+  this.receiveTableForm = function(args) {
+    var schema = args[0];
+    var table = args[1];
+    printMessage('Found table ' + schema.getTableName(table) + '\n');
+    fieldArgs = new Array();
+    fieldArgs[0] = schema;
+    schema.enumFields(table, self.receiveFieldForm, fieldArgs);
+  };
+  this.onSchemaFetchedForm = function(schema) {
+    // printMessage('Request done');
+    var args = new Array();
+    args[0] = schema;
+    schema.enumTables(self.receiveTableForm, args);
+  };
+  this.populateForm = function() {
+    var schema = new DBSchema();
+    schema.fetchSchema(this.onSchemaFetchedForm);
+  };
 }
