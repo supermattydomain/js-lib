b9e294392114d5d28141d08b6325050991f7b75f
(no author) <(no author)@d75fb2d8-f9ea-0310-9219-f9bc96eeea74>
2006-12-25 01:05:09 +0000

    Move some logic about populating schema fields on table from searchform.js to criterion.js
    git-svn-id: https://svn/svn/js-lib@813 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- criterion.js
+++ criterion.js
@@ -6,18 +6,33 @@ function makeOption(label, value) {
   return option;
 };
 
-function SearchCriterion(num, fieldLabels, fields, operations) {
+function SearchCriterion(num, fields, operations) {
+  var self = this;
+  this.criterionNum = num;
   this.init = function() {
     this.div = document.createElement("div");
     this.div.setAttribute("id", "criterion" + num);
     // this.div.style.visible = false;
-    this.div.appendChild(this.makeSelectOptionArray("field" + num, fieldLabels, fields));
+    this.div.appendChild(this.makeSelectOptionArray("field" + num, fields, fields));
     this.div.appendChild(this.makeSelectOptionArray("operation" + num, operations, operations));
     var text_field = document.createElement("input");
     text_field.setAttribute("name", "value" + num);
     text_field.setAttribute("type", "text");
     this.div.appendChild(text_field);
   }
+  this.makeFieldSelectOneTable = function(controlName, schema, table) {
+    var select = document.createElement('select');
+    select.setAttribute("name", controlName);
+    var args = new Array();
+    schema.enumFields(table, function(myargs) {
+      var field = myargs[0];
+      // printMessage('Found field ' + schema.getTableName(table) + '.' + schema.getFieldName(field) + '\n');
+      var label = ucFirst(schema.getTableName(table)) + ' ' + ucFirst(schema.getFieldName(field));
+      var value = schema.getTableName(table) + '.' + schema.getFieldName(field);
+      select.appendChild(makeOption(label, value));
+    }, args);
+    return select;
+  };
   this.makeSelectOptionIter = function(name, optionNameIter, optionValueIter) {
     var select = document.createElement("select");
     select.setAttribute("name", name);
@@ -30,6 +45,21 @@ function SearchCriterion(num, fieldLabels, fields, operations) {
     var optionNameIter = new ArrayIter(optionNames);
     var optionValueIter = new ArrayIter(optionValues);
     return this.makeSelectOptionIter(name, optionNameIter, optionValueIter);
+  };
+  this.populate = function(schema, args) {
+    var tableNum = 0;
+    var args = new Array();
+    schema.enumTables(function(myargs) {
+      var table = myargs[0];
+      if ('vall' != table) {
+	return;
+      }
+      // printMessage('tables[' + tableNum + '] = ' + schema.getTableName(table) + '\n');
+      var fieldSelect = self.makeFieldSelectOneTable('field' + self.criterionNum, schema, table);
+      var operationSelect = self.makeSelectOptionArray('operation' + self.criterionNum, self.operations, operations);
+      self.div.appendChild(fieldSelect);
+      tableNum++;
+    }, args);
   };
   this.getDiv = function() {
     return this.div;
--- searchform.js
+++ searchform.js
@@ -1,7 +1,6 @@
 function SearchForm() {
   this.criteria = new Array();
   this.maxCriteria = 20;
-  this.fieldLabels = null;
   this.operations = new Array(
     'contains',
     'is equal to',
@@ -16,16 +15,6 @@ function SearchForm() {
   this.searchForm = document.getElementById('searchform');
   var self = this;
   this.schema = new DBSchema();
-  this.makeFieldLabels = function() {
-    if (null != this.fieldLabels) {
-      return;
-    }
-    this.fieldLabels = new Array();
-    var i;
-    for (i = 0; i < fields.length; i++) {
-      this.fieldLabels[i] = ucFirstAll(fields[i].replace(/\./g, ' '));
-    }
-  };
   this.makeSearchURL = function() {
     var url = '';
     return url;
@@ -34,8 +23,7 @@ function SearchForm() {
     if (this.criteria.length >= this.maxCriteria) {
 	return;
     }
-    this.makeFieldLabels();
-    var criterion = new SearchCriterion(this.criteria.length, this.fieldLabels, fields, this.operations);
+    var criterion = new SearchCriterion(this.criteria.length, fields, this.operations);
     this.criteria[this.criteria.length] = criterion;
     this.searchForm.appendChild(criterion.getDiv());
   };
@@ -48,28 +36,10 @@ function SearchForm() {
     this.criteria[this.criteria.length - 1] = null;
     this.criteria.length--;
   };
-  this.makeFieldSelectOneTable = function(controlName, table) {
-    var select = document.createElement('select');
-    select.setAttribute("name", controlName);
-    var args = new Array();
-    this.schema.enumFields(table, function(myargs) {
-      var field = myargs[0];
-      // printMessage('Found field ' + self.schema.getTableName(table) + '.' + self.schema.getFieldName(field) + '\n');
-      var label = ucFirst(self.schema.getTableName(table)) + ' ' + ucFirst(self.schema.getFieldName(field));
-      var value = self.schema.getTableName(table) + '.' + self.schema.getFieldName(field);
-      select.appendChild(makeOption(label, value));
-    }, args);
-    return select;
-  };
   this.populate = function(schema) {
-    var tableNum = 0;
     var args = new Array();
-    self.schema.enumTables(function(myargs) {
-      var table = myargs[0];
-      // printMessage('tables[' + tableNum + '] = ' + schema.getTableName(table) + '\n');
-      var select = self.makeFieldSelectOneTable('field' + self.criteria.length, table);
-      self.searchForm.appendChild(select);
-      tableNum++;
+    arrayForAll(self.criteria, function(myargs) {
+      myargs[0].populate(schema);
     }, args);
   };
   this.schema.fetchSchema(this.populate);
