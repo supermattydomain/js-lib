5c9bfa3ec41857fa254cfe9254d78998847f64f6
(no author) <(no author)@d75fb2d8-f9ea-0310-9219-f9bc96eeea74>
2006-12-26 00:48:02 +0000

    Add a ChildIter class which iterates over child nodes of a given node..
    git-svn-id: https://svn/svn/js-lib@857 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- iter.js
+++ iter.js
@@ -13,17 +13,50 @@ function ArrayIter(array) {
   this.iterIndex = 0;
   this.getCount = function() {
     return this.iterArray.length;
-  }
+  };
   this.hasMore = function() {
     return (this.iterIndex < this.iterArray.length);
-  }
+  };
   this.getNext = function() {
-    if (this.iterIndex >= this.iterArray.lenth) {
+    if (this.iterIndex >= this.iterArray.length) {
       return null;
     }
     return this.iterArray[this.iterIndex++];
-  }
+  };
   this.forAll = function(callback, args) {
     arrayForAll(this.iterArray, callback, args);
-  }
+  };
+  this.getIndex = function() {
+    return this.iterIndex;
+  };
+}
+
+function ChildIter(node) {
+  this.iterNode = node;
+  this.currentChild = null;
+  this.iterIndex = 0;
+  this.getCount = function() {
+    if (this.iterNode.hasChildNodes()) {
+      return this.iterNode.childNodes.length;
+    } else {
+      return 0;
+    }
+  };
+  this.hasMore = function() {
+    return (this.currentChild && this.currentChild.nextSibling);
+  };
+  this.getNext = function() {
+    var ret = this.currentChild;
+    if (this.currentChild) {
+      this.currentChild = this.currentChild.nextSibling;
+      this.iterIndex++;
+    }
+    return ret;
+  };
+  this.forAll = function(callback, args) {
+    arrayForAll(this.iterNode.childNodes, callback, args);
+  };
+  this.getIndex = function() {
+    return this.iterIndex;
+  };
 }
