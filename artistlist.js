cc236659195b31a50a457a149ea3807cf555ab76
(no author) <(no author)@d75fb2d8-f9ea-0310-9219-f9bc96eeea74>
2007-06-16 11:52:52 +0000

    Move JS widgets into subdir.
    git-svn-id: https://svn/svn/js-lib@1223 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- artistlist.js
+++
@@ -1,7 +0,0 @@
-function ArtistList() {
-	// showLog('In ArtistList constructor');
-	this.base = TableBrowser;
-	this.base('artist', 'name', new Array('artist.id', 'artist.name'));
-	this.update();
-}
-ArtistList.prototype = new TableBrowser;
