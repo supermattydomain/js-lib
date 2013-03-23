92171fbc36d1b944a2681fc5638e7bf82a2e4159
(no author) <(no author)@d75fb2d8-f9ea-0310-9219-f9bc96eeea74>
2013-03-23 12:27:23 +0000

    Add function that transforms a given point by a given affine transformation matrix.
    git-svn-id: https://svn/svn/js-lib@2780 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- maths.js
+++ maths.js
@@ -7,3 +7,19 @@
 function randomBetween(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+/**
+ * Transform the given point by the given affine transformation matrix
+ * (expressed as an HTML5 Canvas-style array of six reals),
+ * and return the transformed point.
+ * @param x The x co-ordinate of the point
+ * @param y The y co-ordinate of the point
+ * @param m The six-member affine transformation array
+ * @returns {Array} [x, y] The transformed point
+ */
+function transformPoint(x, y, m) {
+	return [
+		m[0] * x + m[2] * y + m[4],
+		m[1] * x + m[3] * y + m[5]
+	];
+}
