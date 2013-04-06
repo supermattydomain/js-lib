1320597a6cfbcf55c2e22e12d7a9462b3e1aabe7
(no author) <(no author)@d75fb2d8-f9ea-0310-9219-f9bc96eeea74>
2013-04-06 02:16:55 +0000

    Use setImmediate if available, and fall back to setTimeout if neither setImmediate nor Window.postMessage are available. This turns this into a shim that implements setZeroTimeout on all browsers. It might be better implemented as a polyfill for setImmediate, however.
    git-svn-id: https://svn/svn/js-lib@2830 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- setZeroTimeout.js
+++ setZeroTimeout.js
@@ -2,31 +2,40 @@
  * window.setZeroTimeout: A version of window.setTimeout that respects a zero timeout argument.
  * Thanks to David Baron.
  * Found at: http://dbaron.org/log/20100309-faster-timeouts
+ * TODO: Turn this setZeroTimeout shim into a polyfill for setImmediate.
  */
 (function() {
 	var timeouts = [];
 	var messageName = "zero-timeout-message";
+	if (setImmediate) {
+		window.setZeroTimeout = setImmediate;
+	} else if (window.postMessage && window.addEventListener) {
+		// Like setTimeout, but only takes a function argument.  There's
+		// no time argument (always zero) and no arguments (you have to
+		// use a closure).
+		function setZeroTimeout(fn) {
+			timeouts.push(fn);
+			window.postMessage(messageName, "*");
+		}
 
-	// Like setTimeout, but only takes a function argument.  There's
-	// no time argument (always zero) and no arguments (you have to
-	// use a closure).
-	function setZeroTimeout(fn) {
-		timeouts.push(fn);
-		window.postMessage(messageName, "*");
-	}
-
-	function handleMessage(event) {
-		if (event.source == window && event.data == messageName) {
-			event.stopPropagation();
-			if (timeouts.length > 0) {
-				var fn = timeouts.shift();
-				fn();
+		function handleMessage(event) {
+			if (event.source == window && event.data == messageName) {
+				event.stopPropagation();
+				if (timeouts.length > 0) {
+					var fn = timeouts.shift();
+					fn();
+				}
 			}
 		}
+
+		window.addEventListener("message", handleMessage, true);
+
+		// Add the one thing we want added to the window object.
+		window.setZeroTimeout = setZeroTimeout;
+	} else {
+		// Fall back to use of setTimeout
+		window.setZeroTimeout = function(func) {
+			setTimeout(func, 0);
+		};
 	}
-
-	window.addEventListener("message", handleMessage, true);
-
-	// Add the one thing we want added to the window object.
-	window.setZeroTimeout = setZeroTimeout;
 })();
