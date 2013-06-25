6f1d79e46b3d36b2a5626cbf2bec3e21a74952f7
(no author) <(no author)@d75fb2d8-f9ea-0310-9219-f9bc96eeea74>
2013-06-25 04:56:14 +0000

    Move all RGB<-->HSL and RGB<-->HSV colour conversion functions into colours.js.
    git-svn-id: https://svn/svn/js-lib@2930 d75fb2d8-f9ea-0310-9219-f9bc96eeea74

--- colours.js
+++ colours.js
@@ -8,92 +8,187 @@ function interpolateColour(c1, c2, amount) {
 }
 
 /**
- * Convert RGB colour to HSV.
- * Based on code found at: http://www.javascripter.net/faq/rgb2hsv.htm
- * @param r Red value, [0..255]
- * @param g Green value, [0..255]
- * @param b Blue value, [0..255]
- * @returns {Array} [ Hue [0..360], Saturation [0..1], Value [0..1] ]
+ * Downloaded 2012-01-15 from:
+ * http://mjijackson.com/2008/02/rgb-to-hsl-and-rgb-to-hsv-color-model-conversion-algorithms-in-javascript
+ *
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the range [0, 255] and
+ * returns h, s, and l in the range [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSL representation
  */
-function rgb2hsv(r, g, b) {
-	var minRGB, maxRGB, d, h, diff;
-	r /= 255;
-	g /= 255;
-	b /= 255;
-	minRGB = Math.min(r, Math.min(g, b));
-	maxRGB = Math.max(r, Math.max(g, b));
-	if (minRGB === maxRGB) {
-		return [ 0, 0, minRGB ]; // Achromatic
-	}
-	// Chromatic
-	d = (r === minRGB) ? g - b : ((b === minRGB) ? r - g : b - r);
-	h = (r === minRGB) ? 3 : ((b === minRGB) ? 1 : 5);
-	diff = maxRGB - minRGB;
-	return [ 60 * (h - d / diff), diff / maxRGB, maxRGB ];
+function rgb2hsl(r, g, b) {
+	var max, min, h, s, l, d;
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    max = Math.max(r, g, b);
+    min = Math.min(r, g, b);
+    l = (max + min) / 2;
+
+    if (max === min) {
+        h = s = 0; // achromatic
+    } else {
+        d = max - min;
+        s = (l > 0.5) ? d / (2 - max - min) : d / (max + min);
+        switch (max) {
+            case r:
+            	h = (g - b) / d + ((g < b) ? 6 : 0);
+            	break;
+            case g:
+            	h = (b - r) / d + 2;
+            	break;
+            default:
+            case b:
+            	h = (r - g) / d + 4;
+            	break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, l];
 }
 
 /**
- * Convert HSV colour to RGB.
- * Based on code found at: http://snipplr.com/view.php?codeview&id=14590
- * Ported from the excellent Java algorithm by Eugene Vishnevsky at:
- * http://www.cs.rit.edu/~ncs/color/t_convert.html
- * @param h Hue, [0..360]
- * @param s Saturation, [0..1]
- * @param v Value, [0..1]
- * @returns {Array} [ Red [0..255], Green [0..255], Blue [0..255] ]
+ * Converts an HSL color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes h, s, and l are contained in the range [0, 1] and
+ * returns r, g, and b in the range [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  l       The lightness
+ * @return  Array           The RGB representation
+ */
+function hsl2rgb(h, s, l) {
+    var r, g, b, p, q;
+
+    if (s === 0) {
+    	q = l; // Silence bogus Eclipse warning.
+    	p = q; // Seems like a bug in its flow control
+        r = p; // analysis.
+        r = g = b = l; // achromatic
+    } else {
+        function hue2rgb(pp, qq, t) {
+            if (t < 0) {
+            	t += 1;
+            }
+            if (t > 1) {
+            	t -= 1;
+            }
+            if (t < 1/6) {
+            	return pp + (qq - pp) * 6 * t;
+            }
+            if (t < 1/2) {
+            	return qq;
+            }
+            if (t < 2/3) {
+            	return pp + (qq - pp) * (2/3 - t) * 6;
+            }
+            return pp;
+        }
+
+        q = (l < 0.5) ? l * (1 + s) : l + s - l * s;
+        p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+
+    return [r * 255, g * 255, b * 255];
+}
+
+/**
+ * Converts an RGB color value to HSV. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes r, g, and b are contained in the range [0, 255] and
+ * returns h, s, and v in the range [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSV representation
+ */
+function rgb2hsv(r, g, b) {
+	var h, s, v, max, min, d;
+    r = r / 255;
+    g = g / 255;
+    b = b / 255;
+    max = Math.max(r, g, b);
+    min = Math.min(r, g, b);
+    v = max;
+
+    d = max - min;
+    s = (max === 0) ? 0 : d / max;
+
+    if (max === min) {
+        h = 0; // achromatic
+    } else {
+        switch(max) {
+            case r:
+            	h = (g - b) / d + (g < b ? 6 : 0);
+            	break;
+            case g:
+            	h = (b - r) / d + 2;
+            	break;
+            case b:
+            default:
+            	h = (r - g) / d + 4;
+            	break;
+        }
+        h /= 6;
+    }
+
+    return [h, s, v];
+}
+
+/**
+ * Converts an HSV color value to RGB. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSV_color_space.
+ * Assumes h, s, and v are contained in the range [0, 1] and
+ * returns r, g, and b in the range [0, 255].
+ *
+ * @param   Number  h       The hue
+ * @param   Number  s       The saturation
+ * @param   Number  v       The value
+ * @return  Array           The RGB representation
  */
 function hsv2rgb(h, s, v) {
-	var r, g, b, i, f, p, q, t;
-	// Normalise hue to [0..360]
-	h %= 360;
-	if (h < 0) {
-		h += 360;
-	}
-	if (s === 0) {
-		// Achromatic (grey)
-		r = g = b = v;
-		return [ Math.round(r * 255), Math.round(g * 255), Math.round(b * 255) ];
-	}
-	h /= 60; // sector 0 to 5
-	i = Math.floor(h);
-	f = h - i; // Fractional part of h
-	p = v * (1 - s);
-	q = v * (1 - s * f);
-	t = v * (1 - s * (1 - f));
-	switch (i) {
-	case 0:
-		r = v;
-		g = t;
-		b = p;
-		break;
-	case 1:
-		r = q;
-		g = v;
-		b = p;
-		break;
-	case 2:
-		r = p;
-		g = v;
-		b = t;
-		break;
-	case 3:
-		r = p;
-		g = q;
-		b = v;
-		break;
-	case 4:
-		r = t;
-		g = p;
-		b = v;
-		break;
-	case 5:
-	default:
-		r = v;
-		g = p;
-		b = q;
-		break;
-	}
-	return [ Math.round(r * 255), Math.round(g * 255), Math.round(b * 255) ];
+    var r, g, b, i, f, p, q, t;
+
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+
+    switch(i % 6) {
+        case 0:
+        	r = v, g = t, b = p;
+        	break;
+        case 1:
+        	r = q, g = v, b = p;
+        	break;
+        case 2:
+        	r = p, g = v, b = t;
+        	break;
+        case 3:
+        	r = p, g = q, b = v;
+        	break;
+        case 4:
+        	r = t, g = p, b = v;
+        	break;
+        case 5:
+        default:
+        	r = v, g = p, b = q;
+        	break;
+    }
+
+    return [r * 255, g * 255, b * 255];
 }
 
 function Colour() {
